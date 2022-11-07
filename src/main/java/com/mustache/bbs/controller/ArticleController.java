0660310d12d278e47f7bf75fb4f27ec966df3efd
ohsuk kwon <percykwon@gmail.com>
2022-11-07 15:56:34 +0900

    UPDATE : Entity, Repository를 이용하여 Controller 코드 수정

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -1,7 +1,10 @@
 package com.mustache.bbs.controller;
 
 import com.mustache.bbs.domain.dto.ArticleDto;
+import com.mustache.bbs.domain.entity.Article;
+import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class ArticleController {
 
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
     @GetMapping(value = "/new")
     public String newArticleForm() {
         return "articles/new";
@@ -20,6 +29,15 @@ public class ArticleController {
     @PostMapping(value = "/posts")
     public String createArticle(ArticleDto form) {
         log.info(form.toString());
-        return "";
+
+        // 1. Dto를 Entity로 변환
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        // 2. Repository에게 Entity를 DB에 저장하게 한다.
+        Article saved = articleRepository.save(articleEntity);
+        log.info(saved.toString());
+
+        return "redirect:/articles/" + saved.getId();
     }
 }
