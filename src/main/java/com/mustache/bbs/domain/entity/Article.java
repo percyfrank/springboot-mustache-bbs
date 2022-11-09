5297faadadf3d68596241935b86789e698afef1e
ohsuk kwon <percykwon@gmail.com>
2022-11-09 11:02:24 +0900

    UPDATE : 테이블 명 추가, toString추가

--- src/main/java/com/mustache/bbs/domain/entity/Article.java
+++ src/main/java/com/mustache/bbs/domain/entity/Article.java
@@ -4,17 +4,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity // 엔티티 어노테이션을 붙여줘야 객체를 인식한다.
+@Table(name = "article2")
 @NoArgsConstructor
 @Getter
 public class Article {
 
     @Id // Entity에는 대표값이 필요하다 대표값을 지정 고유의 값
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
@@ -25,4 +24,8 @@ public class Article {
         this.content = content;
     }
 
+    @Override
+    public String toString() {
+        return "Article{" + "id=" + id + ", title='" + title + '\'' + ", content='" + content + '\'' + '}';
+    }
 }
