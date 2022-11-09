26340b864800d205180c3889f75f43f265a1897e
ohsuk kwon <percykwon@gmail.com>
2022-11-09 11:08:33 +0900

    UPDATE : list화면 추가

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -6,9 +6,14 @@ import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/articles")
@@ -39,5 +44,28 @@ public class ArticleController {
         log.info(saved.toString());
 
         return "redirect:/articles/" + saved.getId();
+    }
+
+    @GetMapping(value = "/{id}")
+    public String selectSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+        if(!optArticle.isEmpty()) {
+            model.addAttribute("article", optArticle.get());
+            return "articles/show";
+        } else {
+            return "articles/error";
+        }
+    }
+
+    @GetMapping(value = "/list")
+    public String list(Model model) {
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "articles/list";
+    }
+
+    @GetMapping(value = "")
+    public String index() {
+        return "redirect:/articles/list";
     }
 }
