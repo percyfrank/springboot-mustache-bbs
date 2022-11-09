cd17b84f1334883b14f2ed11a185eff9315cf1e3
ohsuk kwon <percykwon@gmail.com>
2022-11-09 11:03:20 +0900

    UPDATE : 옵션 변경

--- src/main/java/com/mustache/bbs/repository/ArticleRepository.java
+++ src/main/java/com/mustache/bbs/repository/ArticleRepository.java
@@ -4,4 +4,5 @@ import com.mustache.bbs.domain.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    // <관리대상엔티티, 대표값의 타입(Article의 대표값 id의 타입은 long)> 입력
 }
