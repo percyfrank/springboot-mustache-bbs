15402a71998bdca1dc2ec9784f7d5e7f74dba0fb
ohsuk kwon <percykwon@gmail.com>
2022-11-15 11:52:24 +0900

    ADD : Spring Data JPA 리포지토리 커스텀 메서드

--- src/main/java/com/mustache/bbs/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs/repository/HospitalRepository.java
@@ -3,5 +3,17 @@ package com.mustache.bbs.repository;
 import com.mustache.bbs.domain.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<Hospital,Integer> {
+    List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
+
+    List<Hospital> findByRoadNameAddressContaining(String keyword);
+    List<Hospital> findByHospitalNameStartingWith(String keyword);
+    List<Hospital> findByHospitalNameEndsWith(String keyword);
+
+    List<Hospital> findByPatientRoomCountBetween(int keyword1, int keyword2);
+
+    List<Hospital> findByPatientRoomCountBetweenOrderByPatientRoomCountDesc(int a, int b);
+
 }
