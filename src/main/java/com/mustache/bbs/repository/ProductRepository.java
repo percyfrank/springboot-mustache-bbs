770892aafb58318c55471a627d6fbf4e8017b606
ohsuk kwon <percykwon@gmail.com>
2022-11-16 01:58:10 +0900

    DELETE : Product관련 파일 삭제

--- src/main/java/com/mustache/bbs/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mustache.bbs.repository;
-
-import com.mustache.bbs.domain.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
