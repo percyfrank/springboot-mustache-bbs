011ccb1ab1258172bcb904e0cffc4223a03142e4
ohsuk kwon <percykwon@gmail.com>
2022-11-15 11:50:17 +0900

    UPDATE : 병원 세부 정보 화면 추가

--- src/main/java/com/mustache/bbs/controller/HospitalController.java
+++ src/main/java/com/mustache/bbs/controller/HospitalController.java
@@ -8,9 +8,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/hospitals")
@@ -31,5 +33,16 @@ public class HospitalController {
         model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
         model.addAttribute("next", pageable.next().getPageNumber());
         return "hospitals/list";
+    }
+
+    @GetMapping(value = "/{id}")
+    public String detail(@PathVariable Integer id, Model model) {
+        Optional<Hospital> optdetail = hospitalRepository.findById(id);
+        if(!optdetail.isEmpty()) {
+            model.addAttribute("detail", optdetail.get());
+            return "hospitals/detail";
+        } else {
+            return "hospitals/error";
+        }
     }
 }
