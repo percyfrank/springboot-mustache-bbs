1f5bea6d0afeb8e7f814bf39cd7c014d336ca793
percyfrank <percykwon@gmail.com>
2022-12-11 02:43:54 +0900

    UPDATE : 주석 추가

--- src/main/java/com/mustache/bbs/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs/repository/HospitalRepository.java
@@ -1,6 +1,8 @@
 package com.mustache.bbs.repository;
 
 import com.mustache.bbs.domain.entity.Hospital;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,12 +10,14 @@ import java.util.List;
 public interface HospitalRepository extends JpaRepository<Hospital,Integer> {
     List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
 
+    // 키워드 검색 only, 페이징 안됨
     List<Hospital> findByRoadNameAddressContaining(String keyword);
     List<Hospital> findByHospitalNameStartingWith(String keyword);
     List<Hospital> findByHospitalNameEndsWith(String keyword);
-
     List<Hospital> findByPatientRoomCountBetween(int keyword1, int keyword2);
-
     List<Hospital> findByPatientRoomCountBetweenOrderByPatientRoomCountDesc(int a, int b);
+
+    // 키워드 검색 페이징
+    Page<Hospital> findByRoadNameAddressContaining(String keyword, Pageable pageable);
 
 }
