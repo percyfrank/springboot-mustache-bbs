a8666b18b7acd71caff312652ab78b2909931a9e
percyfrank <percykwon@gmail.com>
2022-12-11 00:33:45 +0900

    UPDATE : 게시글 전체 조회, 게시글 생성 메서드 추가

--- src/main/java/com/mustache/bbs/service/ArticleService.java
+++ src/main/java/com/mustache/bbs/service/ArticleService.java
@@ -1,11 +1,17 @@
 package com.mustache.bbs.service;
 
+import com.mustache.bbs.domain.dto.ArticleAddRequest;
+import com.mustache.bbs.domain.dto.ArticleAddResponse;
 import com.mustache.bbs.domain.dto.ArticleDto;
 import com.mustache.bbs.domain.entity.Article;
 import com.mustache.bbs.repository.ArticleRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ArticleService {
@@ -16,11 +22,25 @@ public class ArticleService {
         this.articleRepository = articleRepository;
     }
 
-    public ArticleDto getArticleById(Long id) {
-        Optional<Article> optArticle = articleRepository.findById(id);
-        Article article = optArticle.get();
-        ArticleDto articeDto = Article.of(article);
+    public List<ArticleDto> findAllArticle(Pageable pageable) {
+        Page<Article> articles = articleRepository.findAll(pageable);
+        return articles.stream().map(ArticleDto::of).collect(Collectors.toList());
+    }
 
-        return articeDto;
+    public ArticleDto getArticleById(Long id) {
+        Article article = articleRepository.findById(id).orElseThrow(() -> new RuntimeException("해당 article이 없습니다."));
+        return ArticleDto.of(article);
+
+//        Optional<Article> optArticle = articleRepository.findById(id);
+//        Article article = optArticle.get();
+//        ArticleDto articeDto = Article.of(article);
+//        return articeDto;
+    }
+
+    public ArticleAddResponse addArticle(ArticleAddRequest articleAddRequest) {
+        Article articleEntity = articleAddRequest.toEntity();
+        Article savedArticle = articleRepository.save(articleEntity);
+
+        return ArticleAddResponse.of(savedArticle);
     }
 }
