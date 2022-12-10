9b7985d0d41a95ff145b1f71a7c4addeb906e459
percyfrank <percykwon@gmail.com>
2022-12-11 00:28:28 +0900

    UDPATE : ObjectMapper추가, 게시글 등록 테스트 추가

--- src/test/java/com/mustache/bbs/controller/api/ArticleRestControllerTest.java
+++ src/test/java/com/mustache/bbs/controller/api/ArticleRestControllerTest.java
@@ -1,20 +1,28 @@
 package com.mustache.bbs.controller.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mustache.bbs.controller.web.ArticleController;
+import com.mustache.bbs.domain.dto.ArticleAddRequest;
+import com.mustache.bbs.domain.dto.ArticleAddResponse;
 import com.mustache.bbs.domain.dto.ArticleDto;
 import com.mustache.bbs.domain.dto.HospitalResponse;
 import com.mustache.bbs.repository.ArticleRepository;
 import com.mustache.bbs.service.ArticleService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,10 +33,14 @@ class ArticleRestControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @MockBean
     ArticleService articleService;
 
     @Test
+    @DisplayName("id의 글이 조회되는가")
     void jsonResponse() throws Exception {
         ArticleDto articleDto = ArticleDto.builder()
                 .id(3L)
@@ -49,4 +61,28 @@ class ArticleRestControllerTest {
 
         verify(articleService).getArticleById(articleId);
     }
+
+    @Test
+    @DisplayName("글 등록이 되는가")
+    void addArticle() throws Exception {
+
+        ArticleAddRequest articleAddRequest = new ArticleAddRequest("제목입니다", "내용입니다.");
+
+        given(articleService.addArticle(articleAddRequest)).willReturn(
+                new ArticleAddResponse(1L,articleAddRequest.getTitle(),articleAddRequest.getContent()));
+
+        mockMvc.perform(post("/api/v1/articles")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(articleAddRequest))
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.title").exists())
+                .andExpect(jsonPath("$.title").value("제목입니다"))
+                .andExpect(jsonPath("$.content").exists())
+                .andDo(print());
+
+        verify(articleService).addArticle(articleAddRequest);
+    }
+
 }
