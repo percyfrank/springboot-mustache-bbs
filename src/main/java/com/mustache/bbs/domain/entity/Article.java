bad682531ea9515515720fa88b7d21b0a8884145
percyfrank <percykwon@gmail.com>
2022-12-11 00:27:48 +0900

    UDPATE : 테이블 어노테이션 삭제, 변환 메서드 삭제

--- src/main/java/com/mustache/bbs/domain/entity/Article.java
+++ src/main/java/com/mustache/bbs/domain/entity/Article.java
@@ -1,20 +1,15 @@
 package com.mustache.bbs.domain.entity;
 
-import com.mustache.bbs.domain.dto.ArticleDto;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity // 엔티티 어노테이션을 붙여줘야 객체를 인식한다.
-@Table(name = "article2")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @ToString
+@Builder
 public class Article {
 
     @Id // Entity에는 대표값이 필요하다 대표값을 지정 고유의 값
@@ -23,9 +18,5 @@ public class Article {
 
     private String title;
     private String content;
-
-    public static ArticleDto of(Article article) {
-        return new ArticleDto(article.getId(), article.getTitle(), article.getContent());
-    }
 
 }
